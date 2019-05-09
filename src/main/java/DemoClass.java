33cb0019e61e1a76ade4a1e0adbc855293d675f2
muhammedk <muhammedk@jfrog.com>
2019-05-09 13:05:16 +0530

    Added Sub

--- src/main/java/DemoClass.java
+++ src/main/java/DemoClass.java
@@ -6,6 +6,7 @@ public class DemoClass {
 
         System.out.println(add(x,y));
         System.out.println(isStr(str));
+        System.out.println(sub(x,y));
     }
     public static int add(int x, int y){
         return x+y;
@@ -13,5 +14,8 @@ public class DemoClass {
     public static boolean isStr(String str){
         if(str.equals("ABCDEFG")) return true;
         return false;
+    }
+    public static int sub(int x, int y){
+        return x-y;
     }
 }
